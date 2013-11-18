99205decb44d082fa91c2ae3e0b22573ae001229
Cyril Blaecke <cyril@cyril.cc>
2013-11-18 12:22:30 +0100

    Home page with countdown

---
+++ assets/js/scripts.js
@@ -0,0 +1,44 @@
+
+jQuery(document).ready(function() {
+
+    /*
+        Background slideshow
+    */
+    $('.coming-soon').backstretch([
+      "assets/img/backgrounds/1.jpg"
+    , "assets/img/backgrounds/2.jpg"
+    , "assets/img/backgrounds/3.jpg"
+    ], {duration: 3000, fade: 750});
+
+    /*
+        Countdown initializer
+    */
+    var now = new Date();
+    var countTo = "2013/11/22 00:00";
+    $('.timer').countdown(countTo, function(event) {
+        var $this = $(this);
+        switch(event.type) {
+            case "seconds":
+            case "minutes":
+            case "hours":
+            case "days":
+            case "weeks":
+            case "daysLeft":
+                $this.find('span.'+event.type).html(event.value);
+                break;
+            case "finished":
+                $this.hide();
+                break;
+        }
+    });
+
+    /*
+        Tooltips
+    */
+    $('.social a.facebook').tooltip();
+    $('.social a.twitter').tooltip();
+    $('.social a.googleplus').tooltip();
+    $('.social a.pinterest').tooltip();
+
+});
+
